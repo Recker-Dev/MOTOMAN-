457e4a71667ea7b69d4ba7e7675e9384ff67a873
Recker <reckerdev@gmail.com>
2023-10-13 17:06:33 +0530

    Added environment variable file

--- app.js
+++ app.js
@@ -1,33 +1,39 @@
-// Import the Express framework
 const express = require("express");
-
-// Create an instance of the Express application
 const app = express();
-
-// Import the HTTPS module
 const https = require("https");
+require('dotenv').config();
 
 // Serve static files from the "public" directory
 app.use(express.static("public"));
 
 app.set("view engine", "ejs");
-
 // Middleware setup
 app.use(express.urlencoded({ extended: true })); // Parse URL-encoded data in the request body
 
-// Define a route to serve the signup.html file
+
+
+const clientID = process.env.CLIENT_ID;
+const clientSecret = process.env.CLIENT_SECRET;
+
+
+
+
+
+
+
+
 app.get("/", function (req, res) {
-  // Use __dirname to get the current directory and directly specify the file path
+  
   res.sendFile(__dirname + "/cleanchan.html");
 });
 
 app.get("/", function (req, res) {
-  // Use __dirname to get the current directory and directly specify the file path
+  
   res.sendFile(__dirname + "/cleanchan.html");
 });
 
 app.get("/boyshostel", function (req, res) {
-  // Use __dirname to get the current directory and directly specify the file path
+  
   res.render(__dirname + "/public/boyshostel.ejs");
 });
 
